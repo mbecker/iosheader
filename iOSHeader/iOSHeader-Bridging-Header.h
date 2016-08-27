505f915a4804c2ba2f40fef17ce0714a0de39a9c
Mats Becker <mbecker@Matss-iMac.local>
2016-08-27 19:03:42 +0200

    updated to new project name

---
+++ iOSHeader/iOSHeader-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  iOSHeader-Bridging-Header.h
+//  iOSHeader
+//
+//  Created by Mats Becker on 8/27/16.
+//  Copyright © 2016 mats.becker@gmail.com. All rights reserved.
+//
+#import "FXBlurView.h"
+#ifndef iOSHeader_Bridging_Header_h
+#define iOSHeader_Bridging_Header_h
+
+
+#endif /* iOSHeader_Bridging_Header_h */
